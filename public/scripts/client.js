856c7b4c1e6f24db00948fe06d33cc22d2bde0ea
Andrew Hastings <droo.hastings@gmail.com>
2017-05-06 17:26:27 -0500

    data appending to DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,12 +12,22 @@ function getListings (){
     success: function(response){
 
       var outputDiv = $('#outputDiv');
-      // outputDiv.empty();
+      outputDiv.empty();
       // console.log(response);
       // $('#outputDiv').append('<p>' + response + '</p>' );
       //for loop w appends
       for (var i = 0; i < response.length; i++) {
-        console.log(response[i]);
+        // console.log(response[i]);
+        // console.log(response[i].cost);
+        var cellText = '<div class=" listDiv col-sm-3 game">';
+        cellText += "<p>" + "Beautiful home in " + response[i].city + "</p>";
+        cellText += "<p>"  + "Featuring " + response[i].sqft + " square feet! </p>";
+        if (response[i].cost ) {
+          cellText += "<p>" + "Buy for only $" + response[i].cost + "! </p></div>";
+        } else {
+          cellText += "<p>" + "Rents for only $" + response[i].rent+ "! </p></div>";
+        }// end else
+        outputDiv.append( cellText);
       }// end for
     }// end success
 
